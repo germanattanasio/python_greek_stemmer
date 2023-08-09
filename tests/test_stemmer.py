d2502404e7e15769357736a42fdd2cf33d9d9fbf
German Attanasio <germanattanasio@gmail.com>
2023-08-09 11:06:42 -0400

    feat: use poetry and update dependencies

--- tests/test_stemmer.py
+++ tests/test_stemmer.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*
-import pytest
 
-import yaml
-from greek_stemmer import GreekStemmer
+from greek_stemmer_plus import GreekStemmer
+
+
+try:
+    from yaml import CLoader as Loader, load as load_yaml
+except ImportError:
+    from yaml import Loader, load as load_yaml
 
 
 def test_stem_examples():
     gs = GreekStemmer()
     words = []
-    with open('tests/fixtures/examples.yml', 'r') as f:
-        words = yaml.load(f.read())
+    with open("tests/fixtures/examples.yml", "r", encoding="utf-8") as f:
+        words = load_yaml(f.read(), Loader=Loader)
 
     for word, st in words.items():
         assert gs.stem(word) == st
@@ -17,4 +21,20 @@ def test_stem_examples():
 
 def test_stem_with_non_greek_letters():
     gs = GreekStemmer()
-    assert gs.stem(u"englishΟΣ") == u"englishΟΣ"
+    assert gs.stem("englishΟΣ") == "englishΟΣ"
+
+
+def test_stem_empty_input():
+    gs = GreekStemmer()
+    assert gs.stem("") == ""
+
+
+def test_stem_with_numbers_symbols():
+    gs = GreekStemmer()
+    assert gs.stem("word123$") == "word123$"
+
+
+def test_stem_long_word():
+    gs = GreekStemmer()
+    long_word = "a" * 1000 + "ΟΣ"
+    assert gs.stem(long_word) == "a" * 1000 + "ΟΣ"
